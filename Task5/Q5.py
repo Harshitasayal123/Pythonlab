a31ba74bb7abe92ff7637ec409ef355865bca41e
Harshita Sayal <sayalharshita12345@gmail.com>
2026-09-01 18:52:49 +0530

    3

--- Task5/Q5.py
+++ Task5/Q5.py
@@ -1,8 +1,8 @@
 #Write a pp to take studnet details like name, roll , number , CGPA and hostel status from the user . TYpecast them into appropriate types and print them along with their detected type
 name = input()
 roll_number = int(input())
-Cgpa = int(input())
-status = input()
+Cgpa = float(input())
+status = bool(input())
 
 print(name, type(name))
 print(roll_number, type(roll_number))
